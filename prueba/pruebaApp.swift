57fa2d56f875e81c4016269f275b12bc5f2bac11
Manuela <manupatino2006@gmail.com>
2023-08-14 09:19:51 -0500

    Initial Commit

---
+++ prueba/pruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaApp.swift
+//  prueba
+//
+//  Created by MacBook Pro on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct pruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
